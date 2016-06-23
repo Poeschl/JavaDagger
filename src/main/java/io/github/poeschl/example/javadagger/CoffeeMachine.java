a70d39302dcec2e734cb135d7807dfa807dc6857
Poeschl <pooeschl_markus@web.de>
2016-06-23 16:59:04 +0200

    Lazy init for the new heater to only init it when needed. And save some "power"

--- src/main/java/io/github/poeschl/example/javadagger/CoffeeMachine.java
+++ src/main/java/io/github/poeschl/example/javadagger/CoffeeMachine.java
@@ -1,25 +1,28 @@
 package io.github.poeschl.example.javadagger;
 
+import dagger.Lazy;
+
 import javax.inject.Inject;
 
 public class CoffeeMachine {
 
-    private final Heater heater;
+    private final Lazy<Heater> heater;
     private final Pump pump;
 
 
     @Inject
-    CoffeeMachine(Heater heater, Pump pump) {
+    CoffeeMachine(Lazy<Heater> heater, Pump pump) {
         this.heater = heater;
         this.pump = pump;
     }
 
     public void brew() {
-        heater.turnOn();
-        if (heater.isHot()) {
+        Heater heaterInstance = heater.get();
+        heaterInstance.turnOn();
+        if (heaterInstance.isHot()) {
             pump.pump();
             System.out.println(" [_]P coffee! [_]P ");
         }
-        heater.turnOff();
+        heaterInstance.turnOff();
     }
 }
